523e7dd52998229c166aa9b50c183d20e7cf0566
shashi <shashi4868kumar@gmail.com>
2025-10-27 16:08:24 +0530

    react-dom version added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import Product from './components/Product'
-import Footer from './components/Footer'
-import {CartProvider, cartProvider} from 'react-use-cart'
-import Cart from './components/Cart.js'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Product from './components/Product';
+import Footer from './components/Footer';
+import { CartProvider } from 'react-use-cart';
+import Cart from './components/Cart.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <CartProvider>
       <Router>
-          <div className="..App">
-           
-            <Routes>
-              <Route exact path="/">
-               <Header/>
-                <div className="main-info" >
-                  <Sidebar/>
-                  <Product/> 
-                </div>
-                <Footer/>
-              </Route>
-      
-              <Route path="/cart">
-              <Cart/>
-              </Route>
-            </Routes>
-      
-    </div>
-        
+        <div className="App">
+          <Routes>
+            {/* Home Page */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <div className="main-info">
+                    <Sidebar />
+                    <Product />
+                  </div>
+                  <Footer />
+                </>
+              }
+            />
+
+            {/* Cart Page */}
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
       </Router>
-  
     </CartProvider>
   );
 }
